13d1259b8664660bdc39929bf5335b040b41656e
Lazaro Tomas Del Prado <lazarodelprado97@gmail.com>
2023-09-14 01:23:05 -0300

    crea ruta y controlador para traer empleado mediante legajo pasado por body

--- back/routes/index.js
+++ back/routes/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import users from './users_route.js'
 import licenses from './licenses_route.js'
+import employees from './employees_route.js'
 
 const router = express.Router()
 /* GET home page. */
@@ -10,5 +11,6 @@ router.get('/', function (req, res, next) {
 
 router.use('/user', users)
 router.use('/license', licenses)
+router.use('/employee', employees)
 
 export default router
