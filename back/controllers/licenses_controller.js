48d447378575db9c85d3278095e099d36dc447ff
Lazaro Tomas Del Prado <lazarodelprado97@gmail.com>
2023-09-19 02:51:40 -0300

    crea un documento de licencia por dia, se separan propiedades de fecha por año, mes y dia

--- back/controllers/licenses_controller.js
+++ back/controllers/licenses_controller.js
@@ -10,14 +10,43 @@ const controller = {
 
     const { fileNumber, typeLicense, startDate, endDate, observation, name, apartDiv, position, keyDate, zone, camp, viaticB, added, uprooting, dedicationOp } = req.body
 
-    const dataLicense = {
-      fileNumber: fileNumber,
-      typeLicense: typeLicense,
-      startDate: startDate,
-      endDate: endDate,
-      observation: observation,
-      userId: user.id
+    let licenseData = []
+
+    function daysOfLicense() {
+      let initialDate = startDate.split('-')
+      let finalDate = endDate.split('-')
+      let initialDateFormat = new Date(initialDate[0], initialDate[1] - 1, initialDate[2])
+      let finalDateFormat = new Date(finalDate[0], finalDate[1] - 1, finalDate[2])
+      let days = Math.floor((finalDateFormat - initialDateFormat) / (1000 * 60 * 60 * 24))
+      console.log(days);
+      for (let i = 0; i < days; i++) {
+        let year = ''
+        let month = ''
+        let day = ''
+        if (initialDateFormat > finalDateFormat) {
+          break
+        }
+        if (initialDateFormat.getDay() !== 0 && initialDateFormat.getDay() !== 6) {
+          year = initialDateFormat.getFullYear()
+          month = initialDateFormat.getMonth() + 1
+          day = initialDateFormat.getDate()
+          let data = {
+            fileNumber: fileNumber,
+            typeLicense: typeLicense,
+            year: year,
+            month: month,
+            day: day,
+            observation: observation,
+            userId: user.id
+          }
+          licenseData.push(data)
+        } else {
+          days = days + 1
+        }
+        initialDateFormat.setDate(initialDateFormat.getDate() + 1)
+      }
     }
+
     const dataEmployee = {
       fileNumber: fileNumber,
       name: name,
@@ -37,14 +66,16 @@ const controller = {
     try {
       const findEmployee = await Employee.findOne({ fileNumber: fileNumber })
       if (!findEmployee) {
+        daysOfLicense()
         const employee = await Employee.create(dataEmployee)
-        const license = await License.create(dataLicense)
+        const license = await License.create(licenseData)
         req.body.success = true
         req.body.sc = 201
         req.body.data = { license, employee }
         defaultResponse(req, res)
       } else {
-        const license = await License.create(dataLicense)
+        daysOfLicense()
+        const license = await License.create(licenseData)
         req.body.success = true
         req.body.sc = 201
         req.body.data = license
