1d344a2551e7be568f64b596466a2fa739a8af5f
Lazaro Tomas Del Prado <lazarodelprado97@gmail.com>
2023-10-17 21:28:49 -0300

    modelo creado

---
+++ back/models/licensesBack_model.js
@@ -0,0 +1,170 @@
+import mongoose from "mongoose"
+
+const schema = new mongoose.Schema({
+  legajo: {
+    type: Number,
+    required: true
+  },
+  nroExpediente: {
+    type: Number,
+    required: true
+  },
+  enfermedadCD: [
+    {
+      diasAcordados: {
+        type: Number,
+        required: true
+      },
+      totalAcumulados: {
+        type: Number,
+        required: true
+      }
+    }
+  ],
+  enfermedadLD: [
+    {
+      diasAcordados: {
+        type: Number,
+        required: true
+      },
+      totalAcumulados: {
+        type: Number,
+        required: true
+      }
+    }
+  ],
+  estudios: [
+    {
+      diasAcordados: {
+        type: Number,
+        required: true
+      },
+      totalAcumulados: {
+        type: Number,
+        required: true
+      }
+    }
+  ],
+  inciso9AnticipoLic: [
+    {
+      diasAcordados: {
+        type: Number,
+        required: true
+      },
+      totalAcumulados: {
+        type: Number,
+        required: true
+      }
+    }
+  ],
+  familiarEnfermo: [
+    {
+      diasAcordados: {
+        type: Number,
+        required: true
+      },
+      totalAcumulados: {
+        type: Number,
+        required: true
+      }
+    }
+  ],
+  anualReglamentaria: [
+    {
+      pendiente: {
+        type: Number,
+        required: true
+      },
+      acordadaPPeriodo: {
+        type: Number,
+        required: true
+      },
+      totalAcumulada: {
+        type: Number,
+        required: true
+      },
+      diasTomados: {
+        type: Number,
+        required: true
+      },
+      saldo: {
+        type: Number,
+        required: true
+      }
+    }
+  ],
+  horasExtras: [
+    {
+      trabHs: {
+        type: Number,
+        required: true
+      },
+      saldoHs: {
+        type: Number,
+        required: true
+      }
+    }
+  ],
+  descansoCompensatorio: [
+    {
+      diasAcordados: {
+        type: Number,
+        required: true
+      },
+      diasTomados: {
+        type: Number,
+        required: true
+      },
+      saldo: {
+        type: Number,
+        required: true
+      },
+      expedienteDeReferencia: {
+        type: Number,
+        required: true
+      }
+    }
+  ],
+  fechaDeUtilizacion: [
+    {
+      desde: [
+        {
+          dia: {
+            type: Number,
+            required: true
+          },
+          mes: {
+            type: Number,
+            required: true
+          },
+          año: {
+            type: Number,
+            required: true
+          }
+        }
+      ],
+      hasta: [
+        {
+          dia: {
+            type: Number,
+            required: true
+          },
+          mes: {
+            type: Number,
+            required: true
+          },
+          año: {
+            type: Number,
+            required: true
+          }
+        }
+      ]
+    }
+  ],
+  observaciones: {
+    type: String,
+    required: false
+  }
+})
+
+export const RegistroDeLicencia = mongoose.model('regDeLicencia', schema)
